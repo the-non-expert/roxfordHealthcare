19a7ce13c819fab80ff6c4bb1bae272ce4e43ec4
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-08-05 13:50:03 +0530

    adding the hero bg to the header

--- src/lib/HomeComponents/Header.svelte
+++ src/lib/HomeComponents/Header.svelte
@@ -1,5 +1,5 @@
 <script>
-  import LogoMain from "../images/RoxfordLogo.jpg";
+  import LogoMain from "../images/roxfordLogo2.png";
 
   let navOptions = [
     { id: 1, name: "Home", redirectUrl: "/" },
@@ -10,7 +10,10 @@
   ];
 </script>
 
-<div class="flex justify-around mt-10 mb-5 items-center headerContainer">
+<div
+  class="headerContainer absolute left-0 right-0 z-10 flex justify-between px-20 gap-10 mt-5 mb-5 items-center bg-white w-10/12
+  mx-auto rounded-xl p-2"
+>
   <div class="w-1/6 logoCont">
     <img src={LogoMain} alt="ROXFORD HEALTHCARE" class="" />
   </div>
@@ -18,43 +21,34 @@
   <header class="flex gap-12 justify-center headerOptions">
     {#each navOptions as item (item.id)}
       <a href={item.redirectUrl}>
-        <p class="text-lg font-Montserrat">{item.name}</p></a
-      >
+        <p class="text-lg font-Montserrat">{item.name}</p>
+      </a>
     {/each}
   </header>
 </div>
 
 <style>
-  @media only screen and (max-width: 639px) {
+  @media only screen and (max-width: 999px) {
     .headerContainer {
+      position: relative;
       flex-direction: column;
       justify-content: center;
       align-items: center;
+      gap: 0;
+      padding: 0;
+      margin-bottom: 10px;
     }
     .logoCont {
-      width: 70%;
+      width: 80%;
       margin-bottom: 10px;
     }
 
     .headerOptions {
-      width: 80%;
+      width: 100%;
       display: flex;
       flex-wrap: wrap;
       justify-content: center;
-      gap: 25px;
-      margin-bottom: 15px;
-    }
-  }
-  @media only screen and (max-width: 999px) {
-    .headerContainer {
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-    }
-
-    .headerOptions {
-      /* gap: 55px; */
-      margin-bottom: 15px;
+      gap: 10px;
     }
   }
 </style>
--- src/lib/HomeComponents/HeroComponent.svelte
+++ src/lib/HomeComponents/HeroComponent.svelte
@@ -2,17 +2,30 @@
   export let data;
 </script>
 
-<div class="relative w-screen h-[70vh]">
+<div class="hero relative w-screen h-[90vh]">
   <img
     src={data.image}
     alt="Banner"
-    class="absolute inset-0 w-full h-full object-cover object-botom"
+    class="absolute inset-0 w-full h-full object-cover object-center"
   />
-  <div class="absolute inset-0 bg-black bg-opacity-60" />
+  <div class="absolute inset-0 bg-black bg-opacity-60"></div>
   <div
-    class="relative flex flex-col items-center justify-center w-full h-full text-center p-10 text-white"
+    class="relative flex flex-col items-center justify-center w-full h-full text-center p-10 text-white hero-content"
   >
     <p class="font-bold text-6xl">{data.tagText}</p>
     <p class="font-semibold p-7 max-w-2xl">{data.supportText}</p>
   </div>
 </div>
+
+<style>
+  .hero {
+    position: relative;
+    z-index: 1;
+  }
+  .hero img {
+    z-index: -1;
+  }
+  .hero-content {
+    z-index: 2;
+  }
+</style>
