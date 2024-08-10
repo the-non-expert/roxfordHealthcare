72e9d7962d7219ec95c548bc0af744be7d1a8eb2
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-08-10 15:41:44 +0530

    respiratory individual popup added

--- src/lib/RespiratoryMedicineList.js
+++ src/lib/RespiratoryMedicineList.js
@@ -5,6 +5,11 @@ export const respiratoryMedicineDetails = [
     composition:
       "Formoterol Fumarate 20 mcg + Budesonide 0.5 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/aerobest05.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contains Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect with Formoterol, a long-acting bronchodilator that dilates respiratory airways. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer for maintenance treatment to prevent or decrease wheezing and troubled breathing caused by asthma or ongoing lung disease such as chronic obstructive pulmonary disease (COPD) along with other lung conditions.",
   },
   {
     id: 2,
@@ -12,48 +17,88 @@ export const respiratoryMedicineDetails = [
     composition:
       "Formoterol Fumarate 20 mcg + Budesonide 1.0 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/aerobest10.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contains Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect with Formoterol, a long-acting bronchodilator that dilates respiratory airways. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer for maintenance treatment to prevent or decrease wheezing and troubled breathing caused by asthma or ongoing lung disease such as chronic obstructive pulmonary disease (COPD) along with other lung conditions.",
   },
   {
     id: 3,
     name: "AEROBEST FB 200 ROXCAPS",
     composition: "Formoterol Fumarate 6 mcg + Budesonide IP 200 mcg",
     imageUrl: "gs://roxford-healthcare.appspot.com/aerobest200.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxcaps",
+    description:
+      "Contains Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect with Formoterol, a long-acting bronchodilator that dilates respiratory airways. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer for maintenance treatment to prevent or decrease wheezing and troubled breathing caused by asthma or ongoing lung disease such as chronic obstructive pulmonary disease (COPD) along with other lung conditions.",
   },
   {
     id: 4,
     name: "AEROBEST FB 400 ROXCAPS",
     composition: "Formoterol Fumarate 6 mcg + Budesonide IP 400 mcg",
     imageUrl: "gs://roxford-healthcare.appspot.com/aerobest400.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxcaps",
+    description:
+      "Contains Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect with Formoterol, a long-acting bronchodilator that dilates respiratory airways. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer for maintenance treatment to prevent or decrease wheezing and troubled breathing caused by asthma or ongoing lung disease such as chronic obstructive pulmonary disease (COPD) along with other lung conditions.",
   },
   {
     id: 5,
     name: "AEROBEST SF 250 ROXCAPS",
     composition: "Salmeterol 50 mcg + Fluticasone Propionate IP 250 mcg",
     imageUrl: "gs://roxford-healthcare.appspot.com/aerobest250.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxcaps",
+    description:
+      "Contains a long acting (12 hours) bronchodilator Salmeterol & anti-inflammatory corticosteroid Fluticasone propionate, which effectively dilates air vessels of lungs for easy flow of air & reduces inflammation for better functioning of lungs. When given by inhalation at recommended doses AEROBEST SF 250 Roxcaps results in reduction of symptoms as well as severity of asthma, with better safety profile compare to orally delivered drugs.",
   },
   {
     id: 6,
     name: "BILSEE 20 TAB",
     composition: "Bilastine 20 mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/bilsee20.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contain Bilastine, an anti-allergic drug that acts by stopping binding of Histamine to its receptor in the body thus stops initiation of allergic reaction. It is prescribed in cases of sneezing, itchy nose, nasal secretion, nasal congestion and red watery eyes along with itchy skin rashes.",
   },
   {
     id: 7,
     name: "BILSEE M TAB",
     composition: "Bilastine 20 mg + Montelukast 10 mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/bilseeM.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Bilastine, an anti-allergic drug that acts by stopping Histamine in the body thus stops allergy & Montelukast, a Leukotriene inhibitor that stops inflammation & swelling and these two salts acts synergistically to provide better results. This combination is prescribed in cases of chronic chest congestion & for better symptomatic relief of seasonal & annual allergies compare to individual drug.",
   },
   {
     id: 8,
     name: "BUDAMIST 0.5 MG ROXPULES",
     composition: "Budesonide IP 0.5 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/budamist05.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contain Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma, COPD & other chronic lung diseases requiring a steroid.",
   },
   {
     id: 9,
     name: "BUDAMIST 1.0 MG ROXPULES",
     composition: "Budesonide IP 1.0 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/budamist10.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contain Budesonide, a corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma, COPD & other chronic lung diseases requiring a steroid.",
   },
   {
     id: 10,
@@ -61,6 +106,11 @@ export const respiratoryMedicineDetails = [
     composition:
       "Levosalbutamol Hydrochloride 1.25 mg + Budesonide 0.5 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/budamistLS.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contains Levosalbutamol, a bronchodilator drug that acts by dilating the airways of the respiratory tract & Budesonide, corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma, COPD & other chronic lung disease conditions.",
   },
   {
     id: 11,
@@ -68,6 +118,11 @@ export const respiratoryMedicineDetails = [
     composition:
       "Levosalbutamol Hydrochloride 0.63 mg + Budesonide IP 0.5 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/BudamistLsKid.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contain Levosalbutamol, a bronchodilator drug that acts by dilating the airways of the respiratory tract & Budesonide, corticosteroid that reduces inflammation and has anti allergic & immuno-modulatory effect. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma, COPD & other chronic lung disease conditions.",
   },
   {
     id: 12,
@@ -75,6 +130,11 @@ export const respiratoryMedicineDetails = [
     composition:
       "Ipratropium Bromide 500 mcg + Levosalbutamol 1.25 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/dualmist.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contains Ipratropium bromide & Levosalbutamol, both are bronchodilator drug that acts by dilating the airways of the respiratory tract. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma & other chronic lung diseases.",
   },
   {
     id: 13,
@@ -82,92 +142,165 @@ export const respiratoryMedicineDetails = [
     composition:
       "Ipratropium Bromide 500 mcg + Levosalbutamol 0.63 mg Respiratory Solution",
     imageUrl: "gs://roxford-healthcare.appspot.com/dualmistLD.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Contains Ipratropium bromide & Levosalbutamol, both are bronchodilator drug that acts by dilating the airways of the respiratory tract. It is prescribed for inhalation i.e. for breathing with the help of Nebulizer to manage asthma & other chronic lung diseases.",
   },
   {
     id: 14,
     name: "FLIPNAC 600 TAB",
     composition: "Acetylcysteine Effervescent 600 mg Tab",
     imageUrl: "gs://roxford-healthcare.appspot.com/Flipnac600.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Acetylcysteine, which is used in the treatment of airway diseases such as bronchitis (inflammation of the mucous membranes in the airways), emphysema, mucoviscidosis (cystic fibrosis) and bronchiectasis (chronic widening of an airway passage).",
   },
   {
     id: 15,
     name: "FLIPNAC INJ",
     composition: "Acetylcysteine USP 200mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Flipnac-injection.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Injections",
+    description:
+      "Used in the treatment of Respiratory disease with excessive mucus, which helps in the loosening of mucus. It eases the symptoms like coughing, chest tightness, breathlessness, and, wheezing.",
   },
   {
     id: 16,
     name: "GLYCOWORD ROXPULES",
     composition: "Glycopyrronium 25mcg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Glycoword.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Roxpules",
+    description:
+      "Used to treat chronic obstructive pulmonary disease (COPD). This medicine helps the muscles around the lungs to stay relaxed and reduces symptoms like coughing, chest tightness and shortness of breath.",
   },
   {
     id: 17,
     name: "NOSENAX NASAL SPRAY",
     composition: "Sodium Chloride 0.65% w/v + Benzalkonium 0.01%",
     imageUrl: "gs://roxford-healthcare.appspot.com/Nosenax.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Spray",
+    description:
+      "Combination of Sodium Chloride and Benzalkonium Chloride which is used for allergy, infection, cold and sneezing. Salt’s chemical name is sodium chloride. Sodium chloride is a medication used to cure or prevent salt loss due to dehydration, excessive perspiration, or other factors. It is used in this case to warm up from the cold. Benzalkonium chloride is commonly used as an eye-drop preservative; at higher concentrations, it is used as an antiseptic and disinfectant.",
   },
   {
     id: 18,
     name: "POVINATOR GARGLE",
     composition: "Povidone Iodine Germicide Gargle 2%",
     imageUrl: "gs://roxford-healthcare.appspot.com/povinator.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Liquids",
+    description:
+      "Contains Povidone Iodine, an antiseptic ingredient that kills disease causing germs to treat. It is prescribed for mucosal Infections of the mouth and throat e.g. gingivitis, mouth ulcers, for oral hygiene prior to, during and after dental and oral surgeries.",
   },
   {
     id: 19,
     name: "ROXHALER DPI DEVICE",
-    composition:
-      "Acebrophylline (SR) 200 mg + Fexofenadine 120 mg + Montelukast 10 mg",
+    composition: "N/A",
     imageUrl: "gs://roxford-healthcare.appspot.com/RoxhalerDPI.png",
+    status: "Prescription Only Device",
+    dosage: "As prescribed by the physician",
+    formulation: "N/A",
   },
   {
     id: 20,
     name: "ROXMONT 3D TAB",
-    composition: "Montelukast 10 mg",
+    composition:
+      "Acebrophylline (SR) 200 mg, Fexofenadine 120 mg & Montelukast 10 mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Roxmont3D.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Montelukast, a Lukotriene inhibitor that relieves inflammation and oedema; Fexofenadine, an anti-allergic that relieves allergy & Acebrophylline in sustain release form which dilates airways of the respiratory tract and removes mucus from them. It is prescribed in cases of long standing asthma, respiratory tract congestion & allergic manifestations & other chronic lung diseases.",
   },
   {
     id: 21,
     name: "ROXMONT 10 TAB",
-    composition: "Montelukast 10mg + Fexofenadine Hydrochloride 120mg",
+    composition: "Montelukast 10mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Roxmont10.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Montelukast, a Leukotriene inhibitor that effectively treat inflammation & oedema associated with allergy and itself also acts as an anti-allergic drug. It is prescribed in case of long standing asthma, chest congestion, seasonal & perennial allergies.",
   },
   {
     id: 22,
     name: "ROXMONT FX TAB",
-    composition: "Montelukast Sodium 10mg + Levocetirizine Hydrochloride 5mg.",
+    composition: "Montelukast 10mg & Fexofenadine Hydrochloride 120mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/RoxmontFX.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Montelukast, a Lukotriene inhibitor that relieves inflammation and oedema & Fexofenadine, an anti-allergic that relieves allergy. It is prescribed in cases of long standing asthma, respiratory tract congestion & all sorts of allergic manifestations.",
   },
   {
     id: 23,
     name: "ROXMONT LC TAB",
     composition: "Montelukast Sodium 10mg + Levocetirizine Hydrochloride 5mg.",
     imageUrl: "gs://roxford-healthcare.appspot.com/RoxmontLC.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablets",
+    description:
+      "Contains Montelukast, a Leukotriene inhibitor that effectively treat inflammation & oedema associated with allergy and Levocetirizine, an anti-allergic drug. It is prescribed in case of long standing asthma, chest congestion, sneezing, itchy nose, nasal secretion, nasal congestion and red watery eyes along with itchy skin rashes.",
   },
   {
     id: 24,
     name: "TECHMIST-AF NASAL SPRAY",
     composition:
-      "Azelastine 0.10% w/v + Fluticasone 0.0357% w/v + Benzalkonium Chloride 0.01% v/v",
+      "Azelastine Hydrochloride 0.140% w/w, Fluticasone Furoate 0.0275% w/w & Benzalkonium Chloride 0.01% w/w",
     imageUrl: "gs://roxford-healthcare.appspot.com/TechmistAF.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Spray",
+    description:
+      "Contains Azelastine, an anti-allergic drug that acts by stopping a chemical called Histamine & Fluticasone, a corticosteroid that stops swelling, redness & pain. It is prescribed to relive severe symptoms of seasonal & annual nasal allergies such as runny, itchy, stuffy nose, sneezing, and post-nasal drip caused by allergies or other conditions.",
   },
   {
     id: 25,
     name: "TECHMIST-F NASAL SPRAY",
     composition:
-      "Fluticasone 0.021% w/v + Benzalkonium Chloride 0.01% v/v + Phenyl Ethyl Alcohol USP 0.25% v/v",
+      "Fluticasone Furoate 0.0275% w/w & Benzalkonium Chloride 0.015% w/w",
     imageUrl: "gs://roxford-healthcare.appspot.com/TechmistF.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Spray",
+    description:
+      "Contains Fluticasone, a corticosteroid that acts by relieving allergy, redness, swelling & stabilizing immunity. It is prescribed to prevent & relieve the symptoms of seasonal & annual allergic complications of nose such as sneezing, runny nose & nasal drip.",
   },
   {
     id: 26,
     name: "YESCORT 30 TAB",
     composition: "Deflazacort 30mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Yescort30.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablet",
+    description:
+      "Contains Deflazacort which is a class of medications called corticosteroids. It is used for treatment of various diseases and conditions such as allergic reactions, asthma, rheumatoid arthritis, certain cancers, auto Immune disorders and other inflammatory disorders.",
   },
   {
     id: 27,
     name: "YESCORT 6 TAB",
     composition: "Deflazacort 6mg",
     imageUrl: "gs://roxford-healthcare.appspot.com/Yexcort6.png",
+    status: "Prescription Only Drug",
+    dosage: "As prescribed by the physician",
+    formulation: "Tablet",
+    description:
+      "Contains Deflazacort which is a class of medications called corticosteroids. It is used for treatment of various diseases and conditions such as allergic reactions, asthma, rheumatoid arthritis, certain cancers, auto Immune disorders and other inflammatory disorders.",
   },
 ];
